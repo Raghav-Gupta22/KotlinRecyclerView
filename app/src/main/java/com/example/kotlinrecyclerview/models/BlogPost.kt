cc4b1b0f0ad0d3270cb69c6a8a3ce7a576dab5f9
Raghav-Gupta22 <raghavgupta216@gmail.com>
2021-05-31 10:21:35 +0530

    Added data class

--- app/src/main/java/com/example/kotlinrecyclerview/models/BlogPost.kt
+++ app/src/main/java/com/example/kotlinrecyclerview/models/BlogPost.kt
@@ -1,5 +1,11 @@
 package com.example.kotlinrecyclerview.models
 
-data class BlogPost {
+data class BlogPost(
+    var title: String,
+    var body: String,
+    var image: String,
+    var username: String
+) {
+
 
 }
